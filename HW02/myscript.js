96b1cbf1a890d53b4d4f67bba5bbdb2e1ad51bb6
dmytrozhuravel <dneprbesr@gmail.com>
2023-03-31 19:25:51 -0400

    while cicle and if

---
+++ HW02/myscript.js
@@ -0,0 +1,9 @@
+let a= Number(prompt("Enter your first number - "));
+let b = Number(prompt("Enter your second number - "));
+let result;
+let operator = prompt("Enter your operator - ");
+
+operator=="+"? console.log( result = a + b): 
+operator=="-"? console.log( result = a - b):
+operator=="*"? console.log( result = a * b): 
+operator=="/" && b == 0? alert ("division by 0 is not posible"):console.log( result = a / b);
---
+++ HW02/myscript1.js
@@ -0,0 +1,7 @@
+let i = 0;
+let a = 20;
+while (i < a){
+    i++
+    if (i%2 == 0) 
+    console.log (i);
+}
