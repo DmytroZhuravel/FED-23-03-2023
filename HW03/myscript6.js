88576402ff9724d5b4f5adbbcf97c15a5fcb8d9a
dmytrozhuravel <dneprbesr@gmail.com>
2023-04-06 20:40:20 -0400

    last cicle

---
+++ HW03/myscript6.js
@@ -0,0 +1,12 @@
+let height = 4;
+let width = 5;
+for (let i = 0; i < height; i++) {
+  let row = '';
+  for (let j = 0; j < width; j++) {
+    row += '*';
+    
+  }
+  console.log ("");
+  console.log(row);
+
+}
